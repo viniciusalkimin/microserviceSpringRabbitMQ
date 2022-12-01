837633435f2c93cf334d040bfb3369af6493e9a0
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2022-12-01 19:53:24 -0300

    Criando estrutura do projeto

---
+++ src/main/java/br/com/alurafood/pagamentos/model/Status.java
@@ -0,0 +1,6 @@
+package br.com.alurafood.pagamentos.model;
+
+public enum Status {
+
+    CRIADO, CONFIRMADO, CANCELADO;
+}
---
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -0,0 +1,54 @@
+package br.com.alurafood.pagamentos.service;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDTO;
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@AllArgsConstructor
+public class PagamentoService {
+
+    private PagamentoRepository repository;
+
+    private ModelMapper mapper;
+
+    public Page<PagamentoDTO> findAll(Pageable paginacao) {
+        return repository
+                .findAll(paginacao).map(p -> mapper.map(p, PagamentoDTO.class));
+    }
+
+    public PagamentoDTO findById(Long id) {
+        Pagamento pagamento = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Objeto não encontrado"));
+
+        return mapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO criarPagamento(PagamentoDTO pagamentoDTO) {
+        var pagamento = mapper.map(pagamentoDTO, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return mapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO atualizarPagamento(Long id, PagamentoDTO pagamentoDTO) {
+        var pagamento = mapper.map(pagamentoDTO, Pagamento.class);
+        pagamento.setId(id);
+        repository.save(pagamento);
+
+        return mapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public void excluirPagamento(Long id) {
+        repository.deleteById(id);
+    }
+}
